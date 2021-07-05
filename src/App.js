a62510ba62f8891ff6580c9f8aec096aba088079
seventty <elmastigeron2015@gmail.com>
2021-07-05 00:15:25 -0400

    Stickers fetched

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react'
 import './App.css';
 import { Stickers } from './components/Stickers';
 import Navbar from './components/Navbar/Navbar';
+import { elites, epicos, miticos } from './components/stickerList';
 
 const App = () => {
   return (
     <>
       <div>
-        <Stickers clasification={"Élites"} colorPalette={{firstColor: "#00cc88", secondColor: "#33ffbb", thirdColor: "rgba(179, 255, 246, 0.75)"}}/>
-        <Stickers clasification={"Épicos"} colorPalette={{firstColor: "#00C4FF", secondColor: "#66dbff", thirdColor: "rgba(102, 219, 255, 0.75)"}}/>
-        <Stickers clasification={"Míticos"} colorPalette={{firstColor: "#FF004C", secondColor: "#FF8E53", thirdColor: "rgba(255, 142, 83, 0.75)"}}/>
+        <Stickers stickerList={elites} clasification={"Élites"} colorPalette={{firstColor: "#00cc88", secondColor: "#33ffbb", thirdColor: "rgba(179, 255, 246, 0.75)"}}/>
+        <Stickers stickerList={epicos} clasification={"Épicos"} colorPalette={{firstColor: "#00C4FF", secondColor: "#66dbff", thirdColor: "rgba(102, 219, 255, 0.75)"}}/>
+        <Stickers stickerList={miticos} clasification={"Míticos"} colorPalette={{firstColor: "#FF004C", secondColor: "#FF8E53", thirdColor: "rgba(255, 142, 83, 0.75)"}}/>
         <Navbar/>
       </div>
     </>
--- src/components/Stickers.jsx
+++ src/components/Stickers.jsx
@@ -32,18 +32,10 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
-const stickers = [
-    { id: 1, name: "HTML", description: "El emblema legendario usado por los nobles combatienes que dieron su vida por el reino de HTML desde tiempos inmemoriales, aquí el escudo de todo aquel que da su vida para mantener los muros de la web levantados y con la frente en alto.", image: "http://ih0.redbubble.net/image.127584817.4804/sticker,375x360.u2.png"},
-    { id: 2, name: "CSS", description: "El emblema legendario usado por los nobles combatienes que dieron su vida por el reino de HTML desde tiempos inmemoriales, aquí el escudo de todo aquel que da su vida para mantener los muros de la web levantados y con la frente en alto.", image: "http://ih0.redbubble.net/image.127584817.4804/sticker,375x360.u2.png"},
-    { id: 3, name: "JavaScript", description: "El emblema legendario usado por los nobles combatienes que dieron su vida por el reino de HTML desde tiempos inmemoriales, aquí el escudo de todo aquel que da su vida para mantener los muros de la web levantados y con la frente en alto.", image: "http://ih0.redbubble.net/image.127584817.4804/sticker,375x360.u2.png"},
-    { id: 4, name: "HTML", description: "El emblema legendario usado por los nobles combatienes que dieron su vida por el reino de HTML desde tiempos inmemoriales, aquí el escudo de todo aquel que da su vida para mantener los muros de la web levantados y con la frente en alto.", image: "http://ih0.redbubble.net/image.127584817.4804/sticker,375x360.u2.png"},
-    { id: 5, name: "CSS", description: "El emblema legendario usado por los nobles combatienes que dieron su vida por el reino de HTML desde tiempos inmemoriales, aquí el escudo de todo aquel que da su vida para mantener los muros de la web levantados y con la frente en alto.", image: "http://ih0.redbubble.net/image.127584817.4804/sticker,375x360.u2.png"},
-    { id: 6, name: "JavaScript", description: "El emblema legendario usado por los nobles combatienes que dieron su vida por el reino de HTML desde tiempos inmemoriales, aquí el escudo de todo aquel que da su vida para mantener los muros de la web levantados y con la frente en alto. El emblema legendario usado por los nobles combatienes que dieron su vida por el reino de HTML desde tiempos inmemoriales, aquí el escudo de todo aquel que da su vida para mantener los muros de la web levantados y con la frente en alto.", image: "http://ih0.redbubble.net/image.127584817.4804/sticker,375x360.u2.png"},
 
-];
-
-export const Stickers = ({ clasification, colorPalette }) => {
+export const Stickers = ({ clasification, colorPalette, stickerList }) => {
     const classes = useStyles(colorPalette);
+    
     return (
         <main className={classes.content}>
             <div className={classes.toolbar}/>
@@ -51,7 +43,7 @@ export const Stickers = ({ clasification, colorPalette }) => {
                 <Typography variant="h5" className={classes.clasificationTitle}>{clasification}</Typography>
             </div>
             <Grid container justify="center" spacing={4}>
-                {stickers.map((sticker) => (
+                {stickerList.map((sticker) => (
                     <Grid item key={sticker.id} xs={12} sm={6} md={4} lg={3}>
                         <Sticker stickers={sticker} colorPalette={colorPalette}/>
                     </Grid>
@@ -60,3 +52,5 @@ export const Stickers = ({ clasification, colorPalette }) => {
         </main>
     )
 }
+
+
