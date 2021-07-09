2fc51ae2ba9a9c635b4a2eea8d6557d0178d9702
seventty <elmastigeron2015@gmail.com>
2021-07-08 21:41:51 -0400

    disclaimer

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ const App = () => {
         <Stickers stickerList={miticos} clasification={"Míticos"} colorPalette={{firstColor: "#FF004C", secondColor: "#FF8E53", thirdColor: "255, 142, 83, 0.75"}}/>
         <Stickers stickerList={gloriaMitica} clasification={"Gloria Mítica"} colorPalette={{firstColor: "#e60073", secondColor: "#ff80bf", thirdColor: "255, 142, 83, 0.75"}} special={true}/>
         <Stickers stickerList={temporada} clasification={"Temporada de Verano 🥭"} colorPalette={{firstColor: "#FDC830", secondColor: "#F37335", thirdColor: "0, 0, 0, 0.75"}} season={true}/>
+        <footer style={{color: "white", background: "black", fontSize: 12, fontFamily: "Nunito", textAlign: "center"}}>Copyright @ Honil Cristina Barinas Núñez, Rainiery Valerio Gonzalez - 2021. Todas las ilustraciones mostradas en la galería son de autoría propia, cualquier reproducción ilegal de este contenido resultará en acciones legales inmediatas.</footer>
         <Navbar/>
       </div>
     </>
